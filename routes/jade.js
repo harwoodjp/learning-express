233290670211ada8c02ef742d03d8bca6357545c
Justin <harwoodjp@g.cofc.edu>
2016-10-17 15:12:39 -0400

    intro added, jade interp/iter moved to jade page

--- routes/jade.js
+++ routes/jade.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var title = 'node+express+jade';
+var tech_stack = ['node', 'express', 'jade'];
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('jade', { 
-		title: title 
+		title: title,
+		tech_stack: tech_stack
 	});
 });
 module.exports = router;
